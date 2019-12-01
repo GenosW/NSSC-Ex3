1dfd847a69a29e26ef0500900e385c38453ec180
GenosW <peter.a.holzner@student.tuwien.ac.at>
2019-12-01 21:33:46 +0100

    first omp trys

--- stenciljacobi/stenciljacobi.cpp
+++ stenciljacobi/stenciljacobi.cpp
@@ -6,12 +6,13 @@
 #include <cmath>
 #include <ctime>
 #include <assert.h>
+#include <omp.h>
 
 using namespace std;
 
 void displayVector(const vector<double>& v, size_t N){
     int limit = 16;
-    if (limit > N){limit = N}
+    if (limit > N){limit = N;}
     for (size_t i = 0; i < limit; i++)
     {
         cout << "\n" << setw(4) << v[i];
@@ -21,7 +22,7 @@ void displayVector(const vector<double>& v, size_t N){
 
 void displayMatrix(vector<double>& A, size_t N){
     int limit = 10;
-    if (limit > N){limit = N}
+    if (limit > N){limit = N;}
     cout << "mat:" << setw(4) << "\n";
     for (size_t i = 0; i < limit; i++)
     {
@@ -35,7 +36,7 @@ void displayMatrix(vector<double>& A, size_t N){
 
 void displayGrid(vector<double>& v, size_t N){
     int limit = 10;
-    if (limit > N){limit = N}
+    if (limit > N){limit = N;}
     cout << setw(8) << setprecision(4) << "\n" ;
     for (size_t i = 0; i < limit; i++)
     {
@@ -64,7 +65,7 @@ double getY(size_t i, size_t N, double h){
 }
 
 int vecVecSub(vector<double>& c,vector<double>& a, vector<double>& b, size_t vec_size){
-    assert(vec_size == c.size() and vec_size == a.size() and vec_size == b.size());
+    assert(vec_size == c.size() && vec_size == a.size() && vec_size == b.size());
     for (size_t i = 0; i < vec_size; i++)
     {
         c[i]=(a[i] - b[i]);
@@ -118,10 +119,10 @@ int checkEq(vector<double>& result, vector<double>& u, vector<double>& b, double
         if (i>(innerLen-1)){
             tempDiff += aij*u[i-innerLen];
         }
-        if (i>0 and i%innerLen!=0){
+        if ((i>0) && (i%innerLen!=0)){
             tempDiff += aij*u[i-1];
         }
-        if (i<(vec_size-1) and (i+1)%innerLen!=0){
+        if (i<(vec_size-1) && (i+1)%innerLen!=0){
             tempDiff += aij*u[i+1];
         }
         if (i<vec_size-innerLen){
@@ -135,10 +136,14 @@ int checkEq(vector<double>& result, vector<double>& u, vector<double>& b, double
 int jacobiMethod(vector<double>& xk, const vector<double>& b, const double aii, const double aij, const int N, const double maxIter){
     const size_t vec_size = xk.size(), innerLen = N-2;
     double temp=0.0;
+    int chunk = innerLen;
     vector<double> xkp1(vec_size, 0); // Vector initialized with 0
+    //# pragma omp for schedule(dynamic, chunk) nowait shared(aij,aii,N,h,k,dh2,up,b)
     for (size_t iteration = 0; iteration < maxIter; iteration++)
     {
         // xkp1 = (b[i] - aij*xk[j])/aii
+        // # pragma omp sections nowait
+        // # pragma omp section
         for (size_t i = 0; i < vec_size; i++) // i is index of vector
         {
             temp = b[i];
@@ -146,11 +151,11 @@ int jacobiMethod(vector<double>& xk, const vector<double>& b, const double aii, 
             {
                 temp = temp - aij*xk[i-innerLen];
             }
-            if (i>0 and i%innerLen!=0)
+            if (i>0 && i%innerLen!=0)
             {
                 temp = temp - aij*xk[i-1]; //left of diag
             }
-            if (i<(vec_size-1) and (i+1)%innerLen!=0)
+            if (i<(vec_size-1) && (i+1)%innerLen!=0)
             {
                 temp = temp - aij*xk[i+1]; //right of diag
             }
@@ -160,10 +165,12 @@ int jacobiMethod(vector<double>& xk, const vector<double>& b, const double aii, 
             }
             xkp1[i] = temp/aii;
         }
-        for (size_t i = 0; i < vec_size; i++)
-        {
-            xk[i] = xkp1[i];
-        }
+        // # pragma omp section
+        xk = xkp1;
+        // for (size_t i = 0; i < vec_size; i++)
+        // {
+        //     xk[i] = xkp1[i];
+        // }
     }
     return 0;
 }     
@@ -171,8 +178,7 @@ int jacobiMethod(vector<double>& xk, const vector<double>& b, const double aii, 
 int main(int argc, char *argv[]){
     size_t N, innerLen, vec_size, maxIterations;
     const double k= 2 * M_PI, k2 = pow(k,2);
-    double h, h2, dh2, x, y, eucNorm, maxNorm, aii, aij, itRuntime = 0.0;
-    vector<double> u, up, A, b, result;
+    double h, h2, dh2, eucNorm, maxNorm, aii, aij, itRuntime = 0.0;
 	struct timespec tItEnd, tItStart;
     //----------------Input----------------//
     assert(argc==3);
@@ -195,29 +201,25 @@ int main(int argc, char *argv[]){
     //Reserve memory
     vec_size = pow(innerLen,2);
     //
-    u.reserve(vec_size);
-    up.reserve(vec_size);
-    b.reserve(vec_size);
-    result.reserve(vec_size);
-    
+    vector<double> u(vec_size, 0.0), up(vec_size, 0.0), b(vec_size, 0.0);
+    omp_set_num_threads(2);
+    int chunk = N-2;
     //Create --> put into function
+    #pragma omp for schedule(dynamic, chunk) nowait shared(N,h,k,dh2,up,b)
     for (size_t i = 0; i < vec_size; i++)
     {
         // Compute x and y depending on index i
         // x = double(i%(innerLen)+1)*h;
         // y = (floor(i/(innerLen))+1)*h;
-        x = getX(i, N, h);
-        y = getY(i, N, h);
-
+        double x = getX(i, N, h);
+        double y = getY(i, N, h);
         // Create vectors
-        u.push_back(0); // Starting value fur u is 0
-        up.push_back(up_func(x, y, k));
-        result.push_back(0);
+        up[i] = up_func(x, y, k);
         if (vec_size-i<=(innerLen)) { // top most row has boundary condition
-            b.push_back((f(x, y, k)+dh2*up_func(x,1,k))); 
+            b[i] = f(x, y, k) + dh2*up_func(x,1,k); 
         }
         else {
-            b.push_back(f(x, y, k));
+            b[i] = f(x, y, k);
         }
     }
     
@@ -228,6 +230,7 @@ int main(int argc, char *argv[]){
     jacobiMethod(u, b, aii, aij, N, maxIterations);
     clock_gettime(CLOCK_REALTIME, &tItEnd);
 
+    vector<double> result(vec_size, 0.0);
     // Compute residual = A*u - b after Jacobi Method
     checkEq(result, u, b, aii, aij, N);
     size_t index_of_max=0;
